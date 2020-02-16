1eaaeba020534bc9bbe0ac5a21570f7946fd71ff
alexeyyevtushok <alexey.yevtushok@gmail.com>
2020-02-16 21:09:28 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
- git push -f git@github.com:alexeyyevtushok/ManagmentAppVueJs.git master:gh-pages
+ git push -f git@github.com:alexeyyevtushok/ManagmentAppVueJs.git master
 
 cd -
